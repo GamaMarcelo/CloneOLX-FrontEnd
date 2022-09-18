660410fd76dbb15ed407f248078cb51a5b68b00e
mistergama <marcelo.allan@gmail.com>
2022-09-18 11:53:58 -0300

    Efetuando formulario login

--- src/Routes.js
+++ src/Routes.js
@@ -3,13 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './Pages/Home';
 import About from './Pages/About';
+import NotFound from './Pages/NotFound';
+import SignIn from './Pages/SignIn'
 
 export default () => {
     return (
         <Routes>
             <Route exact path="/" element={<Home />} />
             <Route exact path="/about" element={<About />} />
-            
+            <Route exact path="/signin" element={<SignIn />} />
+            <Route path="*" element={<NotFound />} />
         </Routes>
     )
 }
